a2c3c49f5c1f1c90506748fcf6308a7ddc77eb46
Laursen <201703930@post.au.dk>
2020-10-01 13:22:49 +0200

    Constify

--- Assignment1/routes/users.js
+++ Assignment1/routes/users.js
@@ -1,23 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const mongoose = require('mongoose');
 const userColl = mongoose.model('User');
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
-  const users = await userColl.find({});
-  console.log(users);
-  res.send(users);
+router.get('/', async function (req, res, next) {
+    const users = await userColl.find({});
+    console.log(users);
+    res.send(users);
 });
 
-router.get('/add', async function(req, res, next) {
-  try {
-    await userColl.create({username: "Glenn", password: "132"});
-    res.send("User added");
-  } catch (e) {
-    res.send(e);
-  }
+router.get('/add', async function (req, res, next) {
+    try {
+        await userColl.create({username: "Glenn", password: "132"});
+        res.send("User added");
+    } catch (e) {
+        res.send(e);
+    }
 });
 
 module.exports = router;
