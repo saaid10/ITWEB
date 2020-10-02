b7bafabfe87fcc9f4e72e6e795441da7d5bdc4f6
Laursen <201703930@post.au.dk>
2020-10-02 16:19:28 +0200

    Things

--- Assignment1/routes/index.js
+++ Assignment1/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    console.log(req.session);
+
     res.render('index', {title: 'Express'});
 });
 
