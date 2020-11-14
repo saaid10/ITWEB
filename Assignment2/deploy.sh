f5b105643db03ea02361d8b29295103b0fd0b816
glennlaursen <glennlaursen@hotmail.com>
2020-11-14 15:20:57 +0100

    Heroku deploy

--- Assignment2/deploy.sh
+++ Assignment2/deploy.sh
@@ -3,10 +3,11 @@ cd Frontend/
 npm install
 npm run build
 cd ../Backend
+npm install
 npm run build
-cd ../
+cd ../../
 git add .
-git add -f Backend/dist/
+git add -f Assignment2/Backend/dist/
 git commit -m "Heroku deploy"
 git push heroku master
 heroku ps:scale web=1
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: cd Assignment2 && cd Backend && npm install && npm run build && npm run start
+web: cd Assignment2 && cd Backend && npm install && npm run start
