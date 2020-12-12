d8df7442ac9c2d804d6e2a51746fea9d51040840
glennlaursen <glennlaursen@hotmail.com>
2020-12-12 16:59:43 +0100

    Delete unnessary stuff

--- Assignment3/backend/src/Server.ts
+++ Assignment3/backend/src/Server.ts
@@ -8,7 +8,6 @@ import 'express-async-errors';
 
 import BaseRouter from './routes';
 import logger from '@shared/Logger';
-import {cookieProps} from '@shared/constants';
 
 import cors from 'cors';
 
@@ -22,16 +21,13 @@ const {BAD_REQUEST} = StatusCodes;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser(cookieProps.secret));
+app.use(cookieParser());
 app.use(cors());
 
 // Show routes called in console during development
-if (process.env.NODE_ENV) {
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
-}
-
-// Security
-if (process.env.NODE_ENV === 'production') {
+} else {
     app.use(helmet());
 }
 
@@ -39,7 +35,6 @@ if (process.env.NODE_ENV === 'production') {
 app.use('/api', BaseRouter);
 
 // Print API errors
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     logger.err(err, true);
     return res.status(BAD_REQUEST).json({
--- Assignment3/backend/src/routes/middleware.ts
+++ Assignment3/backend/src/routes/middleware.ts
@@ -1,35 +1,15 @@
-import StatusCodes from 'http-status-codes';
-import { Request, Response, NextFunction } from 'express';
+import jwt from "express-jwt";
 
-import { UserRoles } from '@entities/User';
-import { cookieProps } from '@shared/constants';
-import { JwtService } from '@shared/JwtService';
+export const authRequired = jwt({
+    secret: process.env.JWT_SECRET as string,
+    algorithms: ['HS256'],
+    requestProperty: 'auth',
+    credentialsRequired: true
+});
 
-
-
-const jwtService = new JwtService();
-const { UNAUTHORIZED } = StatusCodes;
-
-
-// Middleware to verify if user is an admin
-export const adminMW = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        // Get json-web-token
-        const jwt = req.signedCookies[cookieProps.key];
-        if (!jwt) {
-            throw Error('JWT not present in signed cookie.');
-        }
-        // Make sure user role is an admin
-        const clientData = await jwtService.decodeJwt(jwt);
-        if (clientData.role === UserRoles.Admin) {
-            res.locals.userId = clientData.id;
-            next();
-        } else {
-            throw Error('JWT not present in signed cookie.');
-        }
-    } catch (err) {
-        return res.status(UNAUTHORIZED).json({
-            error: err.message,
-        });
-    }
-};
+export const authCredentialsNotRequired = jwt({
+    secret: process.env.JWT_SECRET as string,
+    algorithms: ['HS256'],
+    requestProperty: 'auth',
+    credentialsRequired: false
+});
