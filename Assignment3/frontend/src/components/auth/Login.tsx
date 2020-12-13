a63fa2f6480396d595e5bfa955b57c6ab747ca6c
Sivert Sømme Sagmo <au549312@uni.au.dk>
2020-12-14 00:26:10 +0100

    ee

--- Assignment3/frontend/src/components/auth/Login.tsx
+++ Assignment3/frontend/src/components/auth/Login.tsx
@@ -16,7 +16,6 @@ function Login() {
         console.log(userName, userPassword);
         
         // Something async
-
         const response = await fetch('/api/auth/login', {
                         method: 'POST', 
                         headers: {'Content-Type': 'application/json'}, 
